686746445aa6b4993b9c3f45df9319449ee41f65
personalleandrocesar <leandrocesar.goo@gmail.com>
2024-03-11 18:58:52 -0300

    updt

--- server/api/vitorgarcia/treino/atual/b.ts
+++ server/api/vitorgarcia/treino/atual/b.ts
@@ -1,29 +1,136 @@
 export default defineEventHandler((event) => {
   return [
-    // Série Atual
-    { num: 'Exercício 1', nome: 'Puxada neutra', sets: 3, reps: '12', rest: '50"', grupo: '', obs: '', img: 'https://m.leandrocesar.com/exe/puxadaNeutraFechada.jpg'},
-    { num: 'Exercício 2', nome: 'Remada na polia baixa', sets: 3, reps: '10', rest: '50"', grupo: '', obs: '', img: 'https://m.leandrocesar.com/exe/remadaPoliaBaixa.gif'},
-    { num: 'Exercício 3', nome: 'Remada Máquina', sets: 3, reps: '10 + falha', rest: '50"', grupo: '', obs: 'Fazer 10 com a pegada pronada  + falha na pegada neutra.', img: 'https://m.leandrocesar.com/exe/remadaMaquina.png'},
-    { num: 'Exercício 4', nome: 'Bíceps na polia baixa', sets: 3, reps: '8', rest: '50"', grupo: '', obs: '', img: 'https://m.leandrocesar.com/exe/bicepsPolia.gif'},
-    { num: 'Exercício 5', nome: 'Mesa Flexora', sets: 3, reps: '10', rest: '50"', grupo: '', obs: '', img: 'https://m.leandrocesar.com/exe/mesaFlexora.gif'},
-    { num: 'Exercício 6', nome: 'Abdutora', sets: 3, reps: '15 c/ 1"', rest: '50"', grupo: '', obs: '', img: 'https://m.leandrocesar.com/exe/abdutora.gif'},
-    { num: 'Exercício 7', nome: 'Abdominal Máquina', sets: 3, reps: '10', rest: '50"', grupo: '', obs: '', img: 'https://m.leandrocesar.com/exe/abdominalMaquina.gif'},
-    { num: 'Exercício 8', nome: 'Martelo', sets: 3, reps: '12', rest: '50"', grupo: '', obs: '', img: 'https://m.leandrocesar.com/exe/bicepsMartelo.gif'},
-    { num: 'Exercício 9', nome: 'Abdominal supra', sets: 3, reps: '12', rest: '50"', grupo: '', obs: '', img: 'https://m.leandrocesar.com/exe/abdominalSupra.gif'},
-
-
-    // Modelo:
-    // { num: 'Exercício 1', nome: '', sets: 3, reps: '', rest: '40"', grupo: '', obs: '', img: 'https://m.leandrocesar.com/exe/preImagem.gif'},
-    // { num: 'Exercício 2', nome: '', sets: 3, reps: '', rest: '40"', grupo: '', obs: '', img: 'https://m.leandrocesar.com/exe/preImagem.gif'},
-    // { num: 'Exercício 3', nome: '', sets: 3, reps: '', rest: '40"', grupo: '', obs: '', img: 'https://m.leandrocesar.com/exe/preImagem.gif'},
-    // { num: 'Exercício 4', nome: '', sets: 3, reps: '', rest: '40"', grupo: '', obs: '', img: 'https://m.leandrocesar.com/exe/preImagem.gif'},
-    // { num: 'Exercício 5', nome: '', sets: 3, reps: '', rest: '40"', grupo: '', obs: '', img: 'https://m.leandrocesar.com/exe/preImagem.gif'},
-    // { num: 'Exercício 6', nome: '', sets: 3, reps: '', rest: '40"', grupo: '', obs: '', img: 'https://m.leandrocesar.com/exe/preImagem.gif'},
-    // { num: 'Exercício 7', nome: '', sets: 3, reps: '', rest: '40"', grupo: '', obs: '', img: 'https://m.leandrocesar.com/exe/preImagem.gif'},
-    // { num: 'Exercício 8', nome: '', sets: 3, reps: '', rest: '40"', grupo: '', obs: '', img: 'https://m.leandrocesar.com/exe/preImagem.gif'},
-    // { num: 'Exercício 9', nome: '', sets: 3, reps: '', rest: '40"', grupo: '', obs: '', img: 'https://m.leandrocesar.com/exe/preImagem.gif'},
-    // { num: 'Exercício 10', nome: '', sets: 3, reps: '', rest: '40"', grupo: '', obs: '', img: 'https://m.leandrocesar.com/exe/preImagem.gif'},
-    // { num: 'Exercício 11', nome: '', sets: 3, reps: '', rest: '40"', grupo: '', obs: '', img: 'https://m.leandrocesar.com/exe/preImagem.gif'},
-
+    {
+      "id": 1,
+      "num": "Exercício 1",
+      "nome": "Remada Articulada",
+      "sets": 3,
+      "reps": "10",
+      "rest": "60\"",
+      "grupo": "",
+      "obs": "pegada supinada.",
+      "photo": "lowRow",
+      "img": "https://m.leandrocesar.com/exe/lowRow.gif"
+    },
+    {
+      "id": 2,
+      "num": "Exercício 2",
+      "nome": "Puxada aparelho",
+      "sets": 3,
+      "reps": "15",
+      "rest": "50\"",
+      "grupo": "",
+      "obs": "Pegada pronada",
+      "photo": "puxadaFrente",
+      "img": "https://m.leandrocesar.com/exe/puxadaFrente.gif"
+    },
+    {
+      "id": 3,
+      "num": "Exercício 3",
+      "nome": "Puxada Hammer",
+      "sets": 3,
+      "reps": "10",
+      "rest": "50\"",
+      "grupo": "",
+      "obs": "Pegada Neutra",
+      "photo": " puxadaFrente",
+      "img": "https://m.leandrocesar.com/exe/ puxadaFrente.gif"
+    },
+    {
+      "id": 4,
+      "num": "Exercício 4",
+      "nome": "Crucifixo dorsal (AP)",
+      "sets": 3,
+      "reps": "12",
+      "rest": "60\"",
+      "grupo": "",
+      "obs": "Pegada pronada",
+      "photo": "crucifixoDorsal",
+      "img": "https://m.leandrocesar.com/exe/crucifixoDorsal.gif"
+    },
+    {
+      "id": 5,
+      "num": "Exercício 5",
+      "nome": "Remada alta na polia baixa",
+      "sets": 3,
+      "reps": "12",
+      "rest": "50\"",
+      "grupo": "",
+      "obs": "",
+      "photo": "remadaPoliaBaixa",
+      "img": "https://m.leandrocesar.com/exe/remadaPoliaBaixa.gif"
+    },
+    {
+      "id": 6,
+      "num": "Exercício 6",
+      "nome": "Bíceps no aparelho",
+      "sets": 3,
+      "reps": "10",
+      "rest": "50\"",
+      "grupo": "",
+      "obs": "alternando;",
+      "photo": "bicepsAparelho",
+      "img": "https://m.leandrocesar.com/exe/bicepsAparelho.gif"
+    },
+    {
+      "id": 7,
+      "num": "Exercício 7",
+      "nome": "Bíceps martelo",
+      "sets": 3,
+      "reps": "8",
+      "rest": "50\"",
+      "grupo": "",
+      "obs": "",
+      "photo": "bicepsMartelo",
+      "img": "https://m.leandrocesar.com/exe/bicepsMartelo.gif"
+    },
+    {
+      "id": 8,
+      "num": "Exercício 8",
+      "nome": "Panturrilha em pé no aparelho",
+      "sets": 3,
+      "reps": "15 + 15 curtas",
+      "rest": "50\"",
+      "grupo": "",
+      "obs": "Fazer 15 repetições, 15 curtas em cima!",
+      "photo": "panturrilhaAparelho",
+      "img": "https://m.leandrocesar.com/exe/panturrilhaAparelho.gif"
+    },
+    {
+      "id": 9,
+      "num": "Exercício 9",
+      "nome": "Mesa Flexora",
+      "sets": 3,
+      "reps": "10",
+      "rest": "50\"",
+      "grupo": "",
+      "obs": "",
+      "photo": "mesaFlexora",
+      "img": "https://m.leandrocesar.com/exe/mesaFlexora.gif"
+    },
+    {
+      "id": 10,
+      "num": "Exercício 10",
+      "nome": "Abdutora",
+      "sets": 3,
+      "reps": "10 +10\" +4",
+      "rest": "50\"",
+      "grupo": "",
+      "obs": "Fazer 10 repetições, segurar 10 segundos aberto + 4 repetições.",
+      "photo": "abdutora",
+      "img": "https://m.leandrocesar.com/exe/abdutora.gif"
+    },
+    {
+      "id": 11,
+      "num": "Exercício 11",
+      "nome": "Prancha frontal",
+      "sets": 3,
+      "reps": "30\"",
+      "rest": "50\"",
+      "grupo": "",
+      "obs": "Segurar 30 segundos no mínimo!",
+      "photo": "pranchaFrontal.png",
+      "img": "https://m.leandrocesar.com/exe/pranchaFrontal.png"
+    }
   ]
 })
