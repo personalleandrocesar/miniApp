aec91dde0d7d99c018cec45295ce2a61610bc2d8
personalleandrocesar <leandrocesar.goo@gmail.com>
2024-03-21 18:59:19 -0300

    updt

---
+++ server/api/nirapimentel/avaliacao/one.ts
@@ -0,0 +1,51 @@
+export default defineEventHandler((event) => {
+  return {
+      "data": "10/11/2023",
+      "idade": 39,
+      "massa": 84.1,
+      "altura": 1.53,
+      "nascimeto": "24/06/1984",
+      "sexo": "feminino",
+      "pescoco": 36,
+      "ombro": 115.5,
+      "torax": 111.5,
+      "toraxRelaxado": 111.5,
+      "toraxContraido": 113.5,
+      "cintura": 100,
+      "quadril": 112,
+      // Lado direito
+      "bracoDireitoRelaxado": 34,
+      "bracoDireitoContraido": 35,
+      "antebracoDireito": 27,
+      "coxaMedialDireita": 57.5,
+      "coxaDistalDireita": 51.5,
+      "pernaDireita": 40,
+      // Lado esquerdo
+      "bracoEsquerdoRelaxado": 32.5,
+      "bracoEsquerdoContraido": 36,
+      "antebracoEsquerdo": 26.5,
+      "coxaMedialEsquerda": 58.5,
+      "coxaDistalEsquerda": 53,
+      "pernaEsquerda": 40,
+      // Dobras cutâneas,
+      "dtorax": 11,
+      "tricipital": 34,
+      "subEscapular": 39,
+      "axilarMedia": 28,
+      "abdominal": 39,
+      "supraEspinhal": 37,
+      "coxa": 34,
+      "perna": 17,
+      // Diâmetros ósseos
+      "umero": 7,
+      "punho": 4.9,
+      "femur": 10.3,
+      "tornozelo": 6.7,
+      // Testes
+      "flexaoBraco": "--",
+      "flexaoAbdominal": 16
+      // Postura
+      // Hiperlodose lombar; anteroversão de quadril; pés abdutos bem leve;
+      // 
+    }
+})
